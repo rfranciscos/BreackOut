69bf6967438efbb85ec9161db59dff3cbec9cae0
Renan <renan@prontocombustiveis.com.br>
2021-01-29 00:03:08 -0300

    feat(game): adicionando class Bar

---
+++ src/Bar.ts
@@ -0,0 +1,35 @@
+import { GameArea } from './GameArea';
+import BarImg from '../public/images/breakout_sprites.png';
+
+export class Bar {
+  width: number;
+  height: number;
+  positionX: number;
+  positionY: number;
+  speedX: number;
+  speedY: number;
+  image: HTMLImageElement;
+  gameArea: GameArea;
+
+  constructor(positionX: number, positionY: number, width: number, height: number, gameArea: GameArea) {
+    this.width = width;
+    this.height = height;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.image = new Image();
+    this.image.src = BarImg;
+    this.gameArea = gameArea;
+  }
+
+  refeshPosition = (): void => {
+    const ctx = this.gameArea.context;
+    ctx.drawImage(this.image, this.positionX, this.positionY);
+  };
+
+  setPosition = (): void => {
+    this.positionX += this.speedX;
+    this.positionY += this.speedY;
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { GameArea } from './GameArea';
+import { Bar } from './Bar';
 
-new GameArea().start();
+const gameArea = new GameArea();
+
+const player = new Bar(350, 600, 100, 10, gameArea);
+
+function updateGameArea() {
+  gameArea.clear();
+  player.refeshPosition();
+  player.setPosition();
+}
+
+setInterval(updateGameArea, 20);
+gameArea.start();
