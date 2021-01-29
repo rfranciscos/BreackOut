b08b89ac95cbbbf7124a49aa819457204248c6a9
Renan <renan@prontocombustiveis.com.br>
2021-01-28 23:50:30 -0300

    fix(game): ajustando import de imagens utilizando o webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         use: ['source-map-loader'],
         enforce: 'pre',
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
