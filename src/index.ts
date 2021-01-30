211e8e7d42a8879ce603e4b0b8635ffb359150bc
Renan <renan@prontocombustiveis.com.br>
2021-01-29 22:38:06 -0300

    removendo espaços me branco

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,9 @@ import { Ball } from './Ball';
 import { Game } from './Game';
 
 const gameArea = new GameArea();
-
 const player = new Bar(350, 600, 100, 10, gameArea);
-
 const ball = new Ball(400, 500, 'black', 10, 0, 2 * Math.PI, gameArea, player);
-
 const bricksArea = new BricksArea(13, 6, gameArea, ball);
-
 const game = new Game(ball, gameArea, player, bricksArea);
 
 game.start();
